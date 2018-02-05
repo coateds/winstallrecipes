35043f027fdc4d20e7923a748d36f331fc1cf78f
Dave Coate <coateds@outlook.com>
2018-02-04 19:54:49 -0800

    v0.1.1 Add computer rename functionality

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,10 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures winstallrecipes'
 long_description 'Installs/Configures winstallrecipes'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+
+# v0.1.1 Add computer rename functionality
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- recipes/windows-tweaks.rb
+++ recipes/windows-tweaks.rb
@@ -21,3 +21,40 @@ if node['windows-tweaks']['general-tweaks'].to_s == 'y'
     source 'Windows PowerShell.lnk'
   end
 end
+
+# Here I am going to start introducing PowerShell to the mix
+# Use powershell out to get a single value from powershell into ruby for use later
+# puts "Hostname:"
+# puts powershell_out('$env:COMPUTERNAME').stdout.chop.to_s
+
+# This is an example of a powershell_script resource. In this case the computer is being renamed
+# However, the not_if guard_interpreter prevents the resource from running under certain circumstances
+# Either:
+# 1. The computer is already named as desired (the environment var matches the attribute)
+#    This makes the resource idempotent
+# 2. The attribute has a set value 'no-new-name' that allows the attribute to exist without causing a rename
+# Note that the not_if guard also runs PowerShell, but with slightly different rules(?). Observe the extra escape characters ('\')
+
+ps_shouldrename = <<-EOH
+  return (($env:COMPUTERNAME -eq "#{node['windows-tweaks']['new-computername']}") -or ("#{node['windows-tweaks']['new-computername']}" -eq 'no-new-name'))
+EOH
+
+shouldrename = powershell_out(ps_shouldrename).stdout.chop.to_s
+
+# By moving the PowerShell script out of the resource guard it becomes possible to run ChefSpec tests without stubbing out the command.
+# (Something I have not figured out how to do)
+# The rule seems to be a guard made of Ruby code will not require the stub
+
+powershell_script 'rename-computer' do
+  code "Rename-Computer -NewName #{node['windows-tweaks']['new-computername']}"
+  notifies :reboot_now, 'reboot[restart-computer]', :delayed
+  # notifies :reboot_now, 'reboot[restart-computer]', :immediate
+  # not_if "($env:COMPUTERNAME -eq \'#{node['windows-tweaks']['new-computername']}\') -or (\'#{node['windows-tweaks']['new-computername']}\' -eq 'no-new-name')"
+  not_if { shouldrename == "True" }
+end
+
+# The resource block above uses the notifies option to initiate a reboot
+# It can be either immediate or delayed (wait until the end of the chef run)
+reboot 'restart-computer' do
+  action :nothing
+end
